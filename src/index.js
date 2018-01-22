da397c71c61f58e476826f527b937c31f48c9718
oleksandrmo <Oleksandr.Moroz@playtech.com>
2018-01-22 09:34:54 +0200

    start develop test task

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reducers from 'reducers';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router'
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+const history = syncHistoryWithStore(browserHistory, store);
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={Layout} />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
